595b90940c063b7f95605a77f752025ede80dcbb
Noah Bruderer <noah.bruderer@gmail.com>
2024-08-08 15:11:08 +0200

    removed embedding and llm initiation in the tool
    Former-commit-id: 839ac73992149f946ed18647c8e8b5b5744f72bb

--- baio_workbench/baio/src/mytools/blast/tool.py
+++ baio_workbench/baio/src/mytools/blast/tool.py
@@ -1,7 +1,6 @@
 import os
 
-from langchain.vectorstores import FAISS
-from src.llm import LLM
+from baio.src.mytools.utils import load_vector_store
 
 from . import (
     BLAST_answer,
@@ -10,23 +9,17 @@ from . import (
     submit_blast_query,
 )
 
-llm = LLM.get_instance()
 
-embedding = LLM.get_embedding()
-
-BLAST_db = FAISS.load_local(
-    "./baio/data/persistant_files/vectorstores/BLAST_db_faiss_index", embedding
-)
-
-
-def blast_tool(question: str, embedding, llm, doc):
+def blast_tool(question: str, llm, embedding):
     """BLAST TOOL, use this tool if you need to blast a dna sequence on the blast data
     base on ncbi"""
     log_file_path = "./baio/data/output/BLAST/logfile.json"
     save_file_path = "./baio/data/output/BLAST/files/"
+    path_vdb = "./baio/data/persistant_files/vectorstores/BLAST_db_faiss_index"
     os.makedirs(os.path.dirname(save_file_path), exist_ok=True)
     os.makedirs(os.path.dirname(log_file_path), exist_ok=True)
     # generate api call
+    doc = load_vector_store(path_vdb, embedding)
     query_request = BLAST_api_query_generator(question, doc, llm)
     print(query_request)
     current_uuid = query_request.question_uuid  # Get the UUID of the current request
